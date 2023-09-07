1a9f270596f08d56acf644e30149479485209e17
Mehdi <md1.zouaoui@gmail.com>
2023-09-07 23:30:19 +0200

    Fetching ranked infos from specific user name

--- commands/fun/leagueInfo.js
+++ commands/fun/leagueInfo.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { SlashCommandBuilder } = require("discord.js");
-const apiUrl = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/TOBOZ`;
+const apiUrl = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Naunne`;
 const options = {
   headers: {
     "X-Riot-Token": process.env.LOL_API_KEY,
@@ -13,12 +13,25 @@ const getLeagueDetails = async (user) => {
   try {
     const leagueDetails = await fetch(apiUrl, options);
     const leagueDetailsJSON = await leagueDetails.json();
-    console.log("Summoner :", leagueDetailsJSON);
-    return leagueDetailsJSON;
+    const playerDetails = await getTest(leagueDetailsJSON.id);
+    return playerDetails;
   } catch (error) {
     console.error("Error:", error);
   }
   //   return user;
+};
+
+const getTest = async (summonerId) => {
+  try {
+    const test = await fetch(
+      `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}`,
+      options
+    );
+    const dataJSON = await test.json();
+    return dataJSON;
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 module.exports = {
@@ -28,9 +41,16 @@ module.exports = {
       "Search League of Legend historic infos from a specified name."
     ),
   async execute(interaction) {
-    const userDetails = await getLeagueDetails();
+    const currentPlayerDetails = await getLeagueDetails();
+    const details = currentPlayerDetails[0];
     await interaction.reply(
-      `Invocateur : ${userDetails.name} Niveau de compte : ${userDetails.summonerLevel}`
+      `Invocateur : ${details.summonerName} \nRank: ${details.tier} ${
+        details.rank
+      } \nLP: ${details.leaguePoints} \nWins: ${details.wins} \nLosses ${
+        details.losses
+      } \nWinrate: ${Math.round(
+        (100 * details.wins) / (details.wins + details.losses)
+      )}%`
     );
   },
 };
