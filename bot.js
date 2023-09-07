13c656b05577c188b8e6dc2a3ac4fd952bafb583
Mehdi <md1.zouaoui@gmail.com>
2023-09-07 19:43:34 +0200

    added a script to deploy commands on serveur, corrected interactions event handler

--- bot.js
+++ bot.js
@@ -1,36 +1,37 @@
 require("dotenv").config();
+const { REST, Routes } = require("discord.js");
+
 const fs = require("node:fs");
 const path = require("node:path");
 // Require the necessary discord.js classes
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const token = process.env.DISCORD_TOKEN;
-
+const clientID = process.env.APP_ID;
+const guildID = process.env.SERVEUR_ID;
 // Create a new client instance
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.DirectMessages,
-  ],
+  intents: [GatewayIntentBits.Guilds],
 });
-
 client.commands = new Collection();
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
 
-const commandsPath = path.join(__dirname, "commands"); //Create a path to the commands directory
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js")); //Return an array of all the js file names it contains
-
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ("data" in command && "execute" in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
   }
 }
 
@@ -39,7 +40,7 @@ for (const file of commandFiles) {
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag} `);
   console.log(Events.InteractionCreate);
-  //   console.log(client.commands);
+  console.log(client.commands);
 });
 
 // Log in to Discord with your client's token
@@ -48,11 +49,16 @@ client.login(token);
 //When our bot receiving an interaction
 client.on(Events.InteractionCreate, async (interaction) => {
   // if (!interaction.isChatInputCommand()) return;
+
   const command = interaction.client.commands.get(interaction.commandName);
+
+  console.log("LOOK AT ME THIS IS WHERE YOUR INTERACTION IS SHOWING", command);
+
   if (!command) {
     console.error(`No command matching ${interaction.commandName} was found.`);
     return;
   }
+
   try {
     await command.execute(interaction);
   } catch (error) {
---
+++ commands/fun/leagueInfo.js
@@ -0,0 +1,12 @@
+// const { SlashCommandBuilder } = require("discord.js");
+
+// module.exports = {
+//   data: new SlashCommandBuilder()
+//     .setName("lol")
+//     .setDescription(
+//       "Search League of Legend historic infos from a specified name."
+//     ),
+//   async execute(interaction) {
+//     await interaction.reply("Pong!");
+//   },
+// };
