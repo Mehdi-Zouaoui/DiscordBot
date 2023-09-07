363c8e8ba508b4161ef60275c6e635a8345d5694
Mehdi <md1.zouaoui@gmail.com>
2023-09-07 03:55:04 +0200

    working on receiving instructions form serveur

--- bot.js
+++ bot.js
@@ -1,17 +1,72 @@
-require("dotenv").config()
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const token = process.env.DISCORD_TOKEN
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const token = process.env.DISCORD_TOKEN;
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "commands"); //Create a path to the commands directory
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js")); //Return an array of all the js file names it contains
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag} `);
-    
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag} `);
+  console.log(Events.InteractionCreate);
+  //   console.log(client.commands);
 });
 
 // Log in to Discord with your client's token
 client.login(token);
+
+//When our bot receiving an interaction
+client.on(Events.InteractionCreate, async (interaction) => {
+  // if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
