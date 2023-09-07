a156cdf8d8e4a8cf62bee5bc26f7cbacd729cb6e
Mehdi <md1.zouaoui@gmail.com>
2023-09-07 20:19:00 +0200

    refactored events in directory

--- bot.js
+++ bot.js
@@ -35,44 +35,18 @@ for (const folder of commandFolders) {
   }
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag} `);
-  console.log(Events.InteractionCreate);
-  console.log(client.commands);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js')); // fs.readdirSync().filter() returns an array of all the file names in the given directory and filters for only .js files,
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args)); // callback functions takes args returned by its respective event.
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Log in to Discord with your client's token
 client.login(token);
-
-//When our bot receiving an interaction
-client.on(Events.InteractionCreate, async (interaction) => {
-  // if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  console.log("LOOK AT ME THIS IS WHERE YOUR INTERACTION IS SHOWING", command);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
-});
